cbba549289e90bf5ae405aa0262a388ac922a265
Minako Hanada <hanaminaaa3@gmail.com>
2021-11-10 21:56:47 -0800

    added css and javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+const banner = document.getElementById('banner');
+
+banner.addEventListener('click', () => {
+    banner.classList.toggle('hidden');
+   
+        banner.classList.toggle('cover');
+  
+});
